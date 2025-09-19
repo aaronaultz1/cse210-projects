744ad6ecbee7c1d4b4916f3162c49acbec459399
aaronaultz1 <aul20002@byui.edu>
2025-09-18 20:37:29 -0600

    Complete Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            string userGuess;
+            int guessCount = 0;
+            do
+            {
+
+                Console.Write("What is your guess? ");
+                userGuess = Console.ReadLine();
+                guessCount = 1 + guessCount;
+                if (int.Parse(userGuess) > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (int.Parse(userGuess) < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!\nGuess Count: {guessCount}");
+
+                }
+            } while (int.Parse(userGuess) != magicNumber);
+            Console.WriteLine("Do you want to play again? (y/n): ");
+            response = Console.ReadLine();
+        } while (response == "y");
+        
+        
+
     }
 }
