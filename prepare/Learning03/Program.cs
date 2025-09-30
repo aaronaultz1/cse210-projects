d286ad03148c9487700ef7954ed218af4d71a107
aaronaultz1 <aul20002@byui.edu>
2025-09-29 20:40:10 -0600

    Complete activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -25,12 +25,33 @@ class Program
             _bottom = bottom;
         }
 
+        public string GetFractionString()
+        {
+            return _top + "/" + _bottom;
+        }
+        public double GetDecimalValue()
+        {
+            return (double)_top / _bottom;
+        }
     }
     static void Main()
     {
-        Fraction f1 = new Fraction();
+        Fraction f1 = new Fraction(1);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
         Fraction f2 = new Fraction(5);
-        Fraction f3 = new Fraction(5, 4);
-        Console.WriteLine($"{f1}\n{f2}\n{f3}");
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        
     }
 }
