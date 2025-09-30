1ebd85dfaed0eb0ad40c5a345217fb3eeccb8d33
aaronaultz1 <aul20002@byui.edu>
2025-09-29 20:25:31 -0600

    Step 4 test

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public class Fraction
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Attributes
+        private int _top;
+        private int _bottom;
+
+        // Constructors
+        public Fraction() // No parameters
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+        public Fraction(int top) // Top parameter
+        {
+            _top = top;
+            _bottom = 1;
+        }
+        public Fraction(int top, int bottom) // Top and bottom parameters
+        {
+            _top = top;
+            _bottom = bottom;
+        }
+
+    }
+    static void Main()
+    {
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(5, 4);
+        Console.WriteLine($"{f1}\n{f2}\n{f3}");
     }
 }
